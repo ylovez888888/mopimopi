a0c770ee6bfba352797d4b8933c50546028d586d
HAERUHAERU <ff160212@gmail.com>
2017-10-20 01:14:25 +0900

    Update core.js

--- js/core.js
+++ js/core.js
@@ -357,7 +357,7 @@ function Person(e, p) {
                 this[i] = 0;
             else this[i] = e[i]
         } else {
-            var tmp = parseFloat(e[i].replace(/[,%]+/ig, "")).nanFix().toFixed(underDot);
+            var tmp = parseFloat(e[i].replace(/[, %]+/ig, "")).nanFix().toFixed(underDot);
             if (e[i].indexOf("%") > 0 || Math.floor(tmp) != tmp || e[i].indexOf(".") > 0)
                 this[i] = parseFloat(tmp);
             else
@@ -366,7 +366,7 @@ function Person(e, p) {
     }
     try {
         this.maxhitstr = this.maxhit.split('-')[0];
-        this.maxhitval = this.maxhit.split('-')[1].replace(/,/ig, "")
+        this.maxhitval = parseInt(this.maxhit.split('-')[1].replace(/[,. ]/ig, "")).nanFix()
     } catch (ex) {
         this.maxhit = "?-0";
         this.maxhitstr = "";
@@ -374,7 +374,7 @@ function Person(e, p) {
     }
     try {
         this.maxhealstr = this.maxheal.split('-')[0];
-        this.maxhealval = this.maxheal.split('-')[1].replace(/,/ig, "")
+        this.maxhealval = parseInt(this.maxheal.split('-')[1].replace(/[,. ]/ig, "")).nanFix()
     } catch (ex) {
         this.maxheal = "?-0";
         this.maxhealstr = "";
@@ -695,7 +695,7 @@ function Combatant(e, sortkey) {
                 this.Encounter[i] = 0;
             else this.Encounter[i] = e.detail.Encounter[i]
         } else {
-            var tmp = parseFloat(e.detail.Encounter[i].replace(/[,%]+/ig, "")).nanFix().toFixed(underDot);
+            var tmp = parseFloat(e.detail.Encounter[i].replace(/[, %]+/ig, "")).nanFix().toFixed(underDot);
             if (e.detail.Encounter[i].indexOf("%") > 0 || Math.floor(tmp) != tmp || e.detail.Encounter[i].indexOf(".") > 0)
                 this.Encounter[i] = parseFloat(tmp);
             else this.Encounter[i] = parseInt(tmp).nanFix()
